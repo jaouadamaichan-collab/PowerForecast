fd7ebc2deeabd2e50f0981cf4061ea9f54ca3b84
Alessandro Galleani <alegalleani@gmail.com>
2026-03-10 14:22:34 +0100

    feat: added cleaning methods for basic dataframe (start day to 2017(Croatia) and interpoalte outliers and np.nana

--- power_forecast/logic/get_data/download_api.py
+++ power_forecast/logic/get_data/download_api.py
@@ -20,7 +20,7 @@ def create_dataframe_base(filepath: str) -> pd.DataFrame:
     # Pivot: rows = datetime, columns = country, values = price
     df_pivot = df.pivot_table(
         index='Datetime (UTC)',
-        columns='Country',
+        columns='ISO3 Code',
         values='Price (EUR/MWhe)',
         fill_value = np.nan, 
         aggfunc='first',
@@ -33,6 +33,37 @@ def create_dataframe_base(filepath: str) -> pd.DataFrame:
     
     # Sort index
     df_pivot = df_pivot.sort_index()
+    df_pivot = df_pivot.drop('MKD', axis=1)
     
     return df_pivot
+
+
+def align_start_to_column(df, column, apply=True):
+    """
+    Make dataset start from the first valid (non-NaN) value of a specific column.
+    If Croatia: 2017-10-01 00:00:00
+    """
+    if apply:
+        first_valid = df[column].first_valid_index()
+        df_aligned = df[df.index >= first_valid]
+        print(f"New start: {first_valid} | Dropped {len(df) - len(df_aligned)} rows | {len(df_aligned)} remaining")
+        return df_aligned
+    return df
+
+def replace_outliers_with_interpolation(df, limit_low, limit_high):
+    """
+    Replace values outside [limit_low, limit_high] with NaN,
+    then interpolate all NaNs
+    """
+
+    # Replace outliers with NaN
+    df_clean = df.copy()
+    df_clean[df_clean < limit_low] = float('nan')
+    df_clean[df_clean > limit_high] = float('nan')
+
+    # Interpolate ALL NaNs (pre-existing + new outliers)
+    df_interpolated = df_clean.interpolate(method='time')
+
+    return df_interpolated
+
 
